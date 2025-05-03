50906b93480b367d133db5dd9c95f2078a80f94d
anakon <nazarkinah73@gmail.com>
2025-05-03 10:03:41 +0300

    refactor(web/App): clear root vue file

--- apps/web/src/app/App.vue
+++ apps/web/src/app/App.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-import { useQuery } from '@tanstack/vue-query'
-import { trpc } from '@/shared/lib/trpc'
-import { watch } from 'vue'
 
-const { data, isLoading } = useQuery({
-  queryKey: ['userList'],
-  queryFn: () => trpc.user.userList.query(), // ✅ працює через tRPC клієнт
-})
-
-watch(data, () => console.log(data.value[0]))
 </script>
 
 <template>
   <h1 class="text-3xl font-bold underline">
-    Hello world!
+    123
   </h1>
 </template>
