eaeca362a4f70674eda6fb9511d2da147a23cd62
anakon <nazarkinah73@gmail.com>
2025-06-13 23:38:45 +0300

    feat(trpc): impl routes for each ftp.service's methods

--- packages/trpc/src/routers/ftp.ts
+++ packages/trpc/src/routers/ftp.ts
@@ -12,4 +12,30 @@ export const ftpRouter = router({
     .mutation(async ({ input, ctx }) => {
       return (await ctx).ftp.upload(input.localPath, input.remotePath)
     }),
+  delete: publicProcedure
+    .input(v.object({ path: v.string() }))
+    .mutation(async ({ input, ctx }) => {
+      return (await ctx).ftp.delete(input.path)
+    }),
+  rename: publicProcedure
+    .input(v.object({ oldPath: v.string(), newPath: v.string() }))
+    .mutation(async ({ input, ctx }) => {
+      return (await ctx).ftp.rename(input.oldPath, input.newPath)
+    }),
+  createDirectory: publicProcedure
+    .input(v.object({ path: v.string() }))
+    .mutation(async ({ input, ctx }) => {
+      return (await ctx).ftp.createDirectory(input.path)
+    }),
+  move: publicProcedure
+    .input(v.object({ oldPath: v.string(), newPath: v.string() }))
+    .mutation(async ({ input, ctx }) => {
+      return (await ctx).ftp.move(input.oldPath, input.newPath)
+    }),
+  copy: publicProcedure
+    .input(v.object({ oldPath: v.string(), newPath: v.string() }))
+    .mutation(async ({ input, ctx }) => {
+      return (await ctx).ftp.copy(input.oldPath, input.newPath)
+    }),
+
 })
