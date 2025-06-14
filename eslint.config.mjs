0c35c73c597ecce9340a251784169313f81c4b78
anakon <nazarkinah73@gmail.com>
2025-06-14 13:33:52 +0300

    ci(eslint): add better-tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import antfu from '@antfu/eslint-config'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
 export default antfu({
+  plugins: {
+    'better-tailwindcss': eslintPluginBetterTailwindcss,
+  },
   rules: {
     'max-len': ['error', { code: 80 }],
     'perfectionist/sort-imports': ['off'],
@@ -11,5 +15,16 @@ export default antfu({
     '@typescript-eslint/no-redeclare': 'off',
     'node/prefer-global/process': 'off',
     'node/prefer-global/buffer': 'off',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 3,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+    ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
+    ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+    'better-tailwindcss/multiline': ['warn', { printWidth: 80 }],
   },
 })
