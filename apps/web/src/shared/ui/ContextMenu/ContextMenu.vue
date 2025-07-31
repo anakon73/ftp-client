a76ae0e87c932dde3eadc656814bd246a7628d54
anakon <nazarkinah73@gmail.com>
2025-07-31 15:55:33 +0300

    feat(ContextMenu): impl

---
+++ apps/web/src/shared/ui/ContextMenu/ContextMenu.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {
+  ContextMenuContent,
+  ContextMenuPortal,
+  ContextMenuRoot,
+  ContextMenuTrigger,
+} from 'reka-ui'
+
+interface Slots {
+  default: () => any
+  trigger: () => any
+}
+
+defineSlots<Slots>()
+</script>
+
+<template>
+  <ContextMenuRoot>
+    <ContextMenuTrigger>
+      <slot name="trigger" />
+    </ContextMenuTrigger>
+
+    <ContextMenuPortal>
+      <ContextMenuContent
+        class="
+          min-w-72 rounded-md border border-slate-200 bg-white p-2 shadow-md
+        "
+      >
+        <slot />
+      </ContextMenuContent>
+    </ContextMenuPortal>
+  </ContextMenuRoot>
+</template>
---
+++ apps/web/src/shared/ui/ContextMenu/ContextMenuItem.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ContextMenuItem } from 'reka-ui'
+import type { LucideIcon } from 'lucide-vue-next'
+
+import { cn } from '@/shared/lib/styles'
+
+interface Props {
+  class?: string
+  icon?: LucideIcon
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <ContextMenuItem
+    :class="cn(
+      `
+        cursor-pointer rounded-lg p-1 font-medium text-slate-500
+        transition-colors
+        hover:bg-slate-100 hover:outline-0
+      `,
+      icon ? 'flex items-center gap-2' : '',
+      props.class)"
+  >
+    <component :is="icon" v-if="icon" :size="16" />
+    <slot />
+  </ContextMenuItem>
+</template>
