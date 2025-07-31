bd1ffba515c48f885497a014b3bafb671f938590
anakon <nazarkinah73@gmail.com>
2025-07-31 15:58:09 +0300

    feat(web/app): router provider

--- apps/web/src/app/providers/index.ts
+++ apps/web/src/app/providers/index.ts
@@ -1,6 +1,9 @@
 import type { App } from 'vue'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
+import { router } from './router'
+
 export function applyProviders(app: App) {
   app.use(VueQueryPlugin)
+  app.use(router)
 }
