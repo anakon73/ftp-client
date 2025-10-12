72b77e3a5c705f1e86858ca96b9010b052bad311
anakon <nazarkinah73@gmail.com>
2025-10-12 23:59:45 +0300

    refactor(web/api): delete mutation

--- apps/web/src/modules/ftp/api/index.ts
+++ apps/web/src/modules/ftp/api/index.ts
@@ -1,10 +1,31 @@
-import { trpc } from '@/shared/lib/trpc'
-import { useQuery } from '@tanstack/vue-query'
 import type { Ref } from 'vue'
+import { useMutation, useQuery } from '@tanstack/vue-query'
+import type { FileEntry } from 'packages/trpc'
+
+import { trpc } from '@/shared/lib/trpc'
+import { useRefreshQuery } from '@/shared/api'
+
+const keys = {
+  all: (path: Ref<string>) => (['ftp', path]),
+} as const
 
 export function useFtpList(path: Ref<string>) {
   return useQuery({
-    queryKey: ['ftp', path],
+    queryKey: keys.all(path),
     queryFn: async () => await trpc.ftp.list.query({ path: path.value }),
+  })
+}
+
+export function useFtpDelete(currentPath: Ref<string>) {
+  const { refreshQuery } = useRefreshQuery()
+
+  return useMutation({
+    mutationFn: async (
+      { path, name, type,
+      }: { path: string, name: string, type: FileEntry['type'] }) =>
+      await trpc.ftp.delete.mutate({ path: `${path}/${name}`, type }),
+    onSuccess: () => {
+      refreshQuery(keys.all(currentPath), true)
+    },
   })
 }
