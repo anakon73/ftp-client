78ebf6a6fe7f95d1f380ad6c4354da329cd72588
anakon <nazarkinah73@gmail.com>
2025-10-20 19:13:35 +0300

    feat(web/FtpCreateDir): impl

---
+++ apps/web/src/modules/ftp/ui/create-dir/FtpCreateDir.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref, toRefs } from 'vue'
+import { onKeyDown } from '@vueuse/core'
+import { FolderPlus, Plus } from 'lucide-vue-next'
+
+import { FButton } from '@/shared/ui/FButton'
+import { FDialog } from '@/shared/ui/FDialog'
+import { FInput } from '@/shared/ui/FInput'
+
+import { useFtpCreateDir } from '../../api'
+
+const props = defineProps<{ path: string }>()
+
+const { path } = toRefs(props)
+
+const { mutate } = useFtpCreateDir(path)
+
+const open = ref(false)
+const name = ref('')
+
+function onClose() {
+  open.value = false
+  name.value = ''
+}
+
+function createDir() {
+  if (name.value.length !== 0 && open.value)
+    mutate(name.value, { onSuccess: onClose })
+}
+
+onKeyDown('Enter', createDir)
+</script>
+
+<template>
+  <FButton icon variant="secondary" @click="open = true">
+    <FolderPlus :size="16" />
+  </FButton>
+
+  <FDialog :open="open" @close="onClose">
+    <div class="flex items-center justify-between gap-4">
+      <FInput v-model="name" />
+      <FButton icon variant="success" @click="createDir">
+        <Plus :size="16" />
+      </FButton>
+    </div>
+  </FDialog>
+</template>
---
+++ apps/web/src/modules/ftp/ui/create-dir/index.ts
@@ -0,0 +1,1 @@
+export { default as FtpCreateDir } from './FtpCreateDir.vue'
