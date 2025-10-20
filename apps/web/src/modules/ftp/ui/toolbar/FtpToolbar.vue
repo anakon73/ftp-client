49b4ea64d14f61a05a88e2859657e53ebff4f2bc
anakon <nazarkinah73@gmail.com>
2025-10-20 20:45:42 +0300

    feat(FtpTooltip): use new buttons

--- apps/web/src/modules/ftp/ui/toolbar/FtpToolbar.vue
+++ apps/web/src/modules/ftp/ui/toolbar/FtpToolbar.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { ArrowLeft } from 'lucide-vue-next'
-import { FButton } from '@/shared/ui/FButton'
-
-import { FtpUpload } from '../upload'
+import { FtpRefreshButton } from '../refresh-button'
+import { FtpBackButton } from '../back-button/'
 import { FtpCreateDir } from '../create-dir'
+import { FtpUpload } from '../upload'
 
 defineProps<{ path: string }>()
 defineEmits<{ goBack: [] }>()
@@ -11,14 +10,10 @@ defineEmits<{ goBack: [] }>()
 
 <template>
   <div class="mb-2 flex justify-between">
-    <FButton
-      v-if="path !== '/'"
-      variant="secondary"
-      icon
-      @click="$emit('goBack')"
-    >
-      <ArrowLeft :size="16" />
-    </FButton>
+    <div class="flex gap-2">
+      <FtpRefreshButton :path="path" />
+      <FtpBackButton :path="path" @go-back="$emit('goBack')" />
+    </div>
     <div class="flex gap-2">
       <FtpCreateDir :path="path" />
       <FtpUpload :path="path" />
