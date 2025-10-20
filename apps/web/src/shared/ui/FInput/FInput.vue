1336f94d821de1e57ce0716d475b78258e019933
anakon <nazarkinah73@gmail.com>
2025-10-20 19:12:29 +0300

    feat(web/FInput): impl

---
+++ apps/web/src/shared/ui/FInput/FInput.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { InputHTMLAttributes } from 'vue'
+import { type ClassValue, cn } from '@/shared/lib/styles'
+
+interface Props extends /* @vue-ignore */ InputHTMLAttributes {
+  class?: ClassValue
+}
+
+const props = defineProps<Props>()
+
+const vModel = defineModel()
+</script>
+
+<template>
+  <input
+    v-model="vModel"
+    :="$attrs"
+    type="text"
+    :class="cn(
+      `
+        block w-full rounded border border-gray-600 bg-gray-700 p-2 text-xs
+        text-white placeholder-gray-400
+        focus:border-blue-800 focus:outline-0
+      `,
+      props.class,
+    )"
+  >
+</template>
