33f5115a9119e3e8c529e9c79351ccf99bf8e2ed
anakon <nazarkinah73@gmail.com>
2025-10-20 20:45:27 +0300

    feat(web/FtpRefreshButton): impl

---
+++ apps/web/src/modules/ftp/ui/refresh-button/FtpRefreshButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+import { RefreshCw } from 'lucide-vue-next'
+
+import { useRefreshQuery } from '@/shared/api'
+import { FButton } from '@/shared/ui/FButton'
+import { ftpKeys } from '../../api'
+
+const props = defineProps<{ path: string }>()
+
+const { path } = toRefs(props)
+
+const { refreshQuery } = useRefreshQuery()
+
+function handleClick() {
+  refreshQuery(ftpKeys.all(path), true)
+}
+</script>
+
+<template>
+  <FButton icon variant="secondary" @click="handleClick">
+    <RefreshCw :size="16" />
+  </FButton>
+</template>
