d1a4cadef571b275a5fa0e7e5c6b3fef3fea5320
anakon <nazarkinah73@gmail.com>
2025-10-22 15:32:20 +0300

    feat(web/FInput): increase test size

--- apps/web/src/shared/ui/FInput/FInput.vue
+++ apps/web/src/shared/ui/FInput/FInput.vue
@@ -18,8 +18,8 @@ const vModel = defineModel()
     type="text"
     :class="cn(
       `
-        block w-full rounded border border-gray-600 bg-gray-700 p-2 text-xs
-        text-white placeholder-gray-400
+        block w-full rounded border border-gray-600 bg-gray-700 p-2 text-lg
+        font-medium text-white placeholder-gray-400
         focus:border-blue-800 focus:outline-0
       `,
       props.class,
