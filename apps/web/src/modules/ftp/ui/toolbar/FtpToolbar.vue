3b89d7442a00f876fd1ced885a6dae0a2ff730ab
anakon <nazarkinah73@gmail.com>
2025-10-24 15:42:32 +0300

    feat(web/FtpToolbar): use FtpBreadcrumb

--- apps/web/src/modules/ftp/ui/toolbar/FtpToolbar.vue
+++ apps/web/src/modules/ftp/ui/toolbar/FtpToolbar.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
-import { FtpBackButton, FtpCreateDir, FtpRefreshButton, FtpUpload } from '../'
+import {
+  FtpBackButton,
+  FtpBreadcrumb,
+  FtpCreateDir,
+  FtpRefreshButton,
+  FtpUpload,
+} from '../'
 
 defineProps<{ path: string }>()
-defineEmits<{ goBack: [] }>()
+defineEmits<{ goBack: [], changePath: [path: string] }>()
 </script>
 
 <template>
-  <div class="mb-2 flex justify-between">
-    <div class="flex gap-2">
-      <FtpRefreshButton :path="path" />
-      <FtpBackButton :path="path" @go-back="$emit('goBack')" />
+  <div class="mb-2">
+    <div class="mb-2 flex justify-between">
+      <div class="flex gap-2">
+        <FtpRefreshButton :path="path" />
+        <FtpBackButton :path="path" @go-back="$emit('goBack')" />
+      </div>
+      <div class="flex gap-2">
+        <FtpCreateDir :path="path" />
+        <FtpUpload :path="path" />
+      </div>
     </div>
-    <div class="flex gap-2">
-      <FtpCreateDir :path="path" />
-      <FtpUpload :path="path" />
-    </div>
+    <FtpBreadcrumb :path="path" @change-path="$emit('changePath', $event)" />
   </div>
 </template>
