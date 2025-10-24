12b5508fab45b31c5e9e60fed83a74a50dd831fb
anakon <nazarkinah73@gmail.com>
2025-10-24 19:50:43 +0300

    feat(web/FtpDownload): impl

---
+++ apps/web/src/modules/ftp/ui/download/FtpDownload.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+import { Download } from 'lucide-vue-next'
+
+import { ContextMenuItem } from '@/shared/ui/ContextMenu'
+import { useFtpDownload } from '../../api'
+
+const props = defineProps<{ path: string, name: string }>()
+
+const { path, name } = toRefs(props)
+
+const { mutate } = useFtpDownload(path, name)
+</script>
+
+<template>
+  <ContextMenuItem :icon="Download" @click="mutate">
+    Download
+  </ContextMenuItem>
+</template>
---
+++ apps/web/src/modules/ftp/ui/download/index.ts
@@ -0,0 +1,1 @@
+export { default as FtpDownload } from './FtpDownload.vue'
