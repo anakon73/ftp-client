ade7748f83e1dac4b02f149178205584d49cc707
anakon <nazarkinah73@gmail.com>
2025-10-28 20:33:10 +0200

    build(web): use auto imports plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,22 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'node:path'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    AutoImport({
+      dts: true,
+      imports: [
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+      ],
+    }),
   ],
   resolve: {
     alias: {
