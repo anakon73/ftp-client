50da7dd7b8e3e022f6fe0614554c762f0b9d808c
anakon <nazarkinah73@gmail.com>
2025-10-30 18:03:17 +0200

    build(web/vite): add unplugin-vue-components plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +17,11 @@ export default defineConfig({
         'vue-router',
         '@vueuse/core',
       ],
+    }),
+    Components({
+      dts: true,
+      dirs: ['./src/shared/ui'],
+      extensions: ['vue'],
     }),
   ],
   resolve: {
