d700eadae4dbc554f79c0eea4898964da8c2d92d
anakon <nazarkinah73@gmail.com>
2025-11-05 17:11:13 +0200

    feat(web/routes): add modal for create dir & upload

--- apps/web/src/pages/routes.ts
+++ apps/web/src/pages/routes.ts
@@ -1,4 +1,5 @@
 import type { RouteRecordInfo, RouteRecordRaw } from 'vue-router'
+import { FtpCreateDir, FtpUpload } from '@/modules/ftp'
 import { FtpPage } from './ftp'
 
 export const routes: RouteRecordRaw[] = [
@@ -6,11 +7,25 @@ export const routes: RouteRecordRaw[] = [
     name: 'Ftp',
     path: '/',
     component: FtpPage,
+    children: [
+      {
+        path: 'upload',
+        name: 'FtpUpload',
+        component: FtpUpload,
+      },
+      {
+        path: 'create-dir',
+        name: 'FtpCreateDir',
+        component: FtpCreateDir,
+      },
+    ],
   },
 ]
 
 export interface RouteNamedMap {
   Ftp: RouteRecordInfo<'Ftp', '/'>
+  FtpUpload: RouteRecordInfo<'FtpUpload', '/upload'>
+  FtpCreateDir: RouteRecordInfo<'FtpCreateDir', '/create-dir'>
 }
 
 declare module 'vue-router' {
