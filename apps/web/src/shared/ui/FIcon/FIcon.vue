f3360b200cdddb5025dd7a5b597e5f84e32440aa
anakon <nazarkinah73@gmail.com>
2025-11-05 14:26:39 +0200

    feat(web/FIcon): impl

---
+++ apps/web/src/shared/ui/FIcon/FIcon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { LucideIcon } from 'lucide-vue-next'
+import type { ClassValue } from '@/shared/lib/styles'
+
+const props = withDefaults(
+  defineProps<{ icon: LucideIcon, class?: ClassValue, size?: number }>(),
+  { size: 16 },
+)
+</script>
+
+<template>
+  <component :is="icon" :size :class="props.class" />
+</template>
