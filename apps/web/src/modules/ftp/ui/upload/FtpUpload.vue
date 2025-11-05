bcf4fd2279d85a4a571966ff2deba4df89fbbc4c
anakon <nazarkinah73@gmail.com>
2025-11-05 17:05:45 +0200

    feat(web/upload): add button for apply or cancel upload & get path by composable

--- apps/web/src/modules/ftp/ui/upload/FtpUpload.vue
+++ apps/web/src/modules/ftp/ui/upload/FtpUpload.vue
@@ -1,60 +1,72 @@
 <script setup lang="ts">
 import { Upload } from 'lucide-vue-next'
+import { useModalRoute, usePathParams } from '@/shared/lib/utils'
 import { useFtpUpload } from '../../api'
 
-const props = defineProps<{ path: string }>()
+const { path } = usePathParams()
+const { open, close } = useModalRoute('FtpUpload')
+const { mutate } = useFtpUpload(path)
 
-const { mutate } = useFtpUpload(props.path)
+const file = ref<File | null>(null)
 
-const open = ref(false)
+function setFile(e: Event) {
+  return file.value = (e.target as HTMLInputElement).files?.[0] ?? null
+}
 
-async function onFileChange(e: Event) {
-  const file = (e.target as HTMLInputElement).files?.[0]
-
-  if (!file)
-    return
-
-  mutate(file, { onSuccess: () => open.value = false })
+function submit() {
+  file.value && mutate(file.value, { onSuccess: close })
 }
 </script>
 
 <template>
-  <FButton icon variant="secondary" @click="open = true">
-    <Upload :size="16" />
-  </FButton>
-
-  <FDialog :open @close="open = false">
+  <FDialog :open @close="close">
     <div class="col-span-full">
       <div
         class="
-          mt-2 flex justify-center rounded-lg border border-dashed
-          border-white/25 px-6 py-10
+          flex flex-col items-center justify-center rounded-lg border
+          border-dashed border-white/25 px-6 py-10
         "
       >
-        <div class="text-center">
-          <Upload class="mx-auto size-12 text-gray-600" aria-hidden="true" />
-          <div class="mt-4 flex text-sm/6 text-gray-400">
-            <label
-              for="file-upload"
-              class="
-                relative cursor-pointer rounded-md bg-transparent font-semibold
-                text-indigo-400
-                hover:text-indigo-300
-              "
-            >
-              <span>Upload a file</span>
-              <input
-                id="file-upload"
-                name="file-upload"
-                type="file"
-                class="sr-only"
-                @change="onFileChange"
-              >
-            </label>
-          </div>
-          <p class="text-xs/5 text-gray-400">
-            Size up to 100MB
+        <FIcon
+          :icon="Upload"
+          :size="20"
+          class="mx-auto size-12 text-gray-600"
+          aria-hidden="true"
+        />
+        <label
+          for="file-upload"
+          class="
+            cursor-pointer font-semibold text-indigo-400 transition
+            hover:text-indigo-300
+          "
+        >
+          <span>Upload a file</span>
+          <input
+            id="file-upload"
+            name="file-upload"
+            type="file"
+            class="sr-only"
+            @change="setFile"
+          >
+        </label>
+        <p class="text-xs/5 text-gray-400">
+          Size up to 100MB
+        </p>
+        <div
+          v-if="file"
+          class="mt-2 flex flex-col items-center gap-2 text-center"
+        >
+          <p class="text-sm">
+            {{ file.name }}
           </p>
+          <div class="flex gap-2">
+            <FButton variant="primary" @click="submit">
+              Apply
+            </FButton>
+            <FButton variant="destructive" @click="close">
+              Cancel
+            </FButton>
+          </div>
         </div>
       </div>
     </div>
