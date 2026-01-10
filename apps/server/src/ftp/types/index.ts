fa88e9e9efbcd8a108bac0bbfe936ded300fb57c
anakon <nazarkinah73@gmail.com>
2026-01-10 23:28:52 +0200

    feat(server/ftp/types): add getFile method

--- apps/server/src/ftp/types/index.ts
+++ apps/server/src/ftp/types/index.ts
@@ -1,4 +1,4 @@
-import type { Client } from 'basic-ftp'
+import type { Client, FileInfo } from 'basic-ftp'
 import type { Writable } from 'node:stream'
 
 import type { FileEntry } from '../../types'
@@ -9,6 +9,7 @@ export interface FtpService {
   readonly client: ExtendedFtpClient
   list: (path: string) => Promise<FileEntry[]>
   getEntryType: (path: string) => Promise<FileEntry['type'] | null>
+  getFile: (path: string) => Promise<FileEntry | null>
   download: (remotePath: string, writable: Writable) => Promise<void>
   upload: (remotePath: string, buffer: Buffer) => Promise<void>
   delete: (path: string, type: FileEntry['type']) => Promise<void>
