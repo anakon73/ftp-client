bfb5851037791e73a194912998b7c16315a1f3c9
anakon <nazarkinah73@gmail.com>
2026-01-10 23:54:24 +0200

    feat(web/FtpRename): impl deep work with paths for file & list pages

--- apps/web/src/modules/ftp/ui/rename/FtpRename.vue
+++ apps/web/src/modules/ftp/ui/rename/FtpRename.vue
@@ -1,33 +1,59 @@
 <script setup lang="ts">
+import type { FileEntry } from 'server/types'
 import { Check } from 'lucide-vue-next'
+
 import { useFtpRename } from '../../api'
 
-const props = defineProps<{ name: string, open: boolean, path: string }>()
+const props = defineProps<{
+  item: FileEntry | null
+  open: boolean
+  path: string
+  isFile?: boolean
+}>()
 
-const emits = defineEmits<{ close: [] }>()
+const emits = defineEmits<{ close: [], setPath: [value: string] }>()
 
-const { mutate } = useFtpRename(props.path)
+const { path } = toRefs(props)
 
-const newName = ref(props.name ?? '')
+const name = ref('')
 
-function close() {
-  newName.value = ''
-  emits('close')
+const basePath = computed(
+  () => props.isFile
+    ? path.value.split('/').slice(0, -1).join('/')
+    : path.value,
+)
+
+const targetPath = computed(
+  () => `${basePath.value}/${name.value}`,
+)
+
+const { mutate } = useFtpRename(basePath)
+
+function submit() {
+  mutate(
+    { oldName: props.item?.name ?? '', name },
+    { onSuccess: () => {
+      emits('close')
+      emits('setPath', targetPath.value)
+    } },
+  )
 }
 
-function rename() {
-  mutate({ oldName: props.name, newName }, { onSuccess: () => emits('close') })
-}
+watchEffect(() => {
+  if (props.open)
+    name.value = props.item?.name ?? ''
+})
 </script>
 
 <template>
-  <FDialog :open @close="close">
+  <FDialog :open @close="emits('close')">
     <FDialogTitle class="mb-2">
       Rename
     </FDialogTitle>
+    <p />
     <div class="flex items-center justify-between gap-4">
-      <FInput v-model="newName" />
-      <FButton icon variant="success" @click="rename">
+      <FInput v-model="name" />
+      <FButton icon variant="success" @click="submit">
         <FIcon :icon="Check" stroke-width="2" />
       </FButton>
     </div>
