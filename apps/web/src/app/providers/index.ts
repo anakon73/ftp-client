7fc6037b79d65f6664880aaa5c7af94385a267cd
anakon <nazarkinah73@gmail.com>
2026-01-10 23:46:32 +0200

    feat(web/app): add pinia

--- apps/web/src/app/providers/index.ts
+++ apps/web/src/app/providers/index.ts
@@ -3,7 +3,10 @@ import { VueQueryPlugin } from '@tanstack/vue-query'
 
 import { router } from './router'
 
+const pinia = createPinia()
+
 export function applyProviders(app: App) {
   app.use(VueQueryPlugin)
+  app.use(pinia)
   app.use(router)
 }
